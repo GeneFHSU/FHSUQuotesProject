7ba3ef23887da0b7f98cf25cc012c20aef0bebf4
Eugene <e_kramer2@mail.fhsu.edu>
2021-05-11 01:57:31 -0700

    Put Update.

--- api/quotes/update.php
+++ api/quotes/update.php
@@ -5,43 +5,28 @@ header('Access-Control-Allow-Methods: PUT');
 
 require_once('../../model/quotes_db.php');
 
-if($_SERVER['REQUEST_METHOD'] != 'PUT') exit();
-
-//Process the PUT parameters
-//https://joshtronic.com/2014/06/01/how-to-process-put-requests-with-php/
-if ($_SERVER['REQUEST_METHOD'] == 'PUT')
-{
-    if($_SERVER["CONTENT_TYPE"] == 'application/x-www-form-urlencoded') {
-        parse_str(file_get_contents("php://input"), $_PUT);
-        foreach ($_PUT as $key => $value) {
-            unset($_PUT[$key]);
-            $_PUT[str_replace('amp;', '', $key)] = $value;
-        }
-        $_REQUEST = array_merge($_REQUEST, $_PUT);
-    }
-    else if($_SERVER["CONTENT_TYPE"] == 'text/plain'){
-        $_PUT = json_decode(file_get_contents("php://input"));
-        var_dump($_PUT);
-        $_REQUEST = array_merge($_REQUEST, $_PUT);
-
-    }
-    else{
-        echo json_encode(array('error' => 'Invalid encoding.'));
-        exit();
-    }
+if($_SERVER['REQUEST_METHOD'] != 'PUT'){
+    echo json_encode(array('error' => 'Invalid request method.'));
+    exit();
+}
+if($_SERVER["CONTENT_TYPE"] != 'text/plain'){
+    echo json_encode(array('error' => 'Invalid content-type encoding.'));
+    exit();
 }
 
+$_PUT = json_decode(file_get_contents("php://input"));
+
 //Verify all parameters present
-if(!isset($_REQUEST["id"]) || !isset($_REQUEST["quote"]) || !isset($_REQUEST["authorId"]) || !isset($_REQUEST["categoryId"]))
+if(!isset($_PUT["id"]) || !isset($_PUT["quote"]) || !isset($_PUT["authorId"]) || !isset($_PUT["categoryId"]))
 {
     echo json_encode(array('error' => 'A parameter is missing.'));
     exit();
 }
 //Sanitize the PUT parameters
-$id = filter_var( $_REQUEST["id"], FILTER_SANITIZE_NUMBER_INT);
-$quote = filter_var( $_REQUEST["quote"], FILTER_SANITIZE_STRING);
-$authorId = filter_var( $_REQUEST["authorId"], FILTER_SANITIZE_NUMBER_INT);
-$categoryId = filter_var(($_REQUEST["categoryId"]), FILTER_SANITIZE_NUMBER_INT);
+$id = filter_var( $_PUT["id"], FILTER_SANITIZE_NUMBER_INT);
+$quote = filter_var( $_PUT["quote"], FILTER_SANITIZE_STRING);
+$authorId = filter_var( $_PUT["authorId"], FILTER_SANITIZE_NUMBER_INT);
+$categoryId = filter_var(($_PUT["categoryId"]), FILTER_SANITIZE_NUMBER_INT);
 
 //Verify all parameters present
 if (empty($id) || empty($quote) || empty($authorId) || empty($categoryId))
