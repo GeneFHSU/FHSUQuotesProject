0566f501f64147993e3aae5e2e3e2ec4c6e0aa52
Eugene <e_kramer2@mail.fhsu.edu>
2021-05-11 01:38:27 -0700

    Put Update.

--- api/quotes/update.php
+++ api/quotes/update.php
@@ -5,19 +5,28 @@ header('Access-Control-Allow-Methods: PUT');
 
 require_once('../../model/quotes_db.php');
 
-//if($_SERVER['REQUEST_METHOD'] != 'PUT') exit();
+if($_SERVER['REQUEST_METHOD'] != 'PUT') exit();
 
 //Process the PUT parameters
 //https://joshtronic.com/2014/06/01/how-to-process-put-requests-with-php/
 if ($_SERVER['REQUEST_METHOD'] == 'PUT')
 {
-    parse_str(file_get_contents("php://input"), $_PUT);
-    foreach ($_PUT as $key => $value)
-    {
-        unset($_PUT[$key]);
-        $_PUT[str_replace('amp;', '', $key)] = $value;
+    if($_SERVER["CONTENT_TYPE"] == 'application/x-www-form-urlencoded') {
+        parse_str(file_get_contents("php://input"), $_PUT);
+        foreach ($_PUT as $key => $value) {
+            unset($_PUT[$key]);
+            $_PUT[str_replace('amp;', '', $key)] = $value;
+        }
+        $_REQUEST = array_merge($_REQUEST, $_PUT);
     }
-    $_REQUEST = array_merge($_REQUEST, $_PUT);
+    else if($_SERVER["CONTENT_TYPE"] == 'text/plain'){
+        $_PUT = json_decode(file_get_contents("php://input"));
+        $_REQUEST = array_merge($_REQUEST, $_PUT);
+    }
+    else{
+        echo json_encode(array('error' => 'Invalid encoding.'));
+        exit();
+    }
 }
 
 //Sanitize the PUT parameters
@@ -27,11 +36,12 @@ $authorId = filter_var( $_REQUEST["authorId"], FILTER_SANITIZE_NUMBER_INT);
 $categoryId = filter_var( $_REQUEST["categoryId"], FILTER_SANITIZE_NUMBER_INT);
 
 //Debug
+/*
 $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_STRING);
 $quote = filter_input(INPUT_POST, 'quote', FILTER_SANITIZE_STRING);
 $authorId = filter_input(INPUT_POST, 'authorId', FILTER_SANITIZE_STRING);
 $categoryId = filter_input(INPUT_POST, 'categoryId', FILTER_SANITIZE_STRING);
-
+*/
 
 //Verify all parameters present
 if (empty($id) || empty($quote) || empty($authorId) || empty($categoryId))
