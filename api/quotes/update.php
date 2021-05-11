956d837f4951f0798759604cfb0b7f6e899c358e
Eugene <e_kramer2@mail.fhsu.edu>
2021-05-11 02:00:22 -0700

    Put Update.

--- api/quotes/update.php
+++ api/quotes/update.php
@@ -35,6 +35,8 @@ if (empty($id) || empty($quote) || empty($authorId) || empty($categoryId))
     echo json_encode(array('error' => 'A parameter is missing.'));
 else {
     //Update the quote in the database
+    echo "updating";
     $quote = QuoteDB::update_quote($id, $quote, $authorId, $categoryId) ?? array("message"=>"Quote Updated");
+    echo "updated";
     echo json_encode($quote);
 }
